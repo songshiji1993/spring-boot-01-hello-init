ad2ba3052c1fad532450dab2e13858e896a87342
song <songshiji@bupt.edu.cn>
2019-01-21 23:52:45 +0800

    AML语法格式 key: value 表示一对键值对（冒号后面必须要有空格） 使用空格缩进表示层级关系 左侧缩进的空格数目不重要，只要同一层级的元素左侧对齐即可 key 与 value 大小写敏感 字面量： 数值，字符串，布尔，日期 字符串 默认不用加上引号； “” 使用 双引号 不会转义特殊字符，特殊字符最终会转成本来想表示含义输出 name: "mengxuegu \n jiaoyu" 输出： mengxuegu 换行 jiaoyu '' 使用 单引号 会转义特殊字符，特殊字符当作一个普通的字符串输出 name: 'mengxuegu \n jiaoyu 输出： mengxuegu \n jiaoyu 对象 & Map key: value value存储对象，每个值换一行写，注意值要左对齐

---
+++ src/main/java/com/song01/springboot/SpringBoot01HelloInitApplication.java
@@ -0,0 +1,21 @@
+package com.song01.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class   SpringBoot01HelloInitApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBoot01HelloInitApplication.class, args);
+    }
+
+
+
+
+
+
+
+}
+
